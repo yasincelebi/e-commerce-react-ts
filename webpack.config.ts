9a44345231d09cab94aab6f8be2449d124f85c7a
yasincelebi <yasincelebix@gmail.com>
2021-04-18 17:27:28 +0300

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,62 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+module.exports = {
+  mode: "none",
+  entry: {
+    app: path.join(__dirname, "src", "index.tsx"),
+  },
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/img/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "static/fonts/[name].[hash:7].[ext]",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "typings-for-css-modules-loader",
+            options: {
+              namedexport: true,
+              camelcase: true,
+              modules: true,
+            },
+          },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
